3d316171a84fa5fddb4239b4858424ce9a82b1a1
amprem8 <amprem8@gmail.com>
2024-04-27 00:53:17 +0530

    Updates

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "personal-4yy",
+    project: "iphone"
+  })],
+
+  build: {
+    sourcemap: true
+  }
+})
